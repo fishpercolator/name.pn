e939ccb6c0093525668b8775299a70f59823ce01
Quinn Daley <quinn@fishpercolator.co.uk>
2020-05-05 18:33:28 +0100

    Add the first pieces of a user dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
   skip_after_action :verify_policy_scoped
   skip_after_action :verify_authorized
+  
   def index
+    if user_signed_in?
+      render 'user_home'
+    else
+      render 'anon_home'
+    end
   end
 
   def manifest
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,14 @@
 module HomeHelper
+  
+  def you_must
+    content_tag(:span, class: 'icon has-text-warning is-medium') do
+      content_tag(:i, '', class: 'mdi mdi-24px mdi-alert')
+    end +
+    content_tag(:span, t('home.user_home.you_must'), class: 'has-text-warning has-text-weight-bold')
+  end
+  
+  def not_set
+    content_tag(:span, t('home.user_home.not_set'), class: 'is-italic has-text-grey')
+  end
+  
 end
