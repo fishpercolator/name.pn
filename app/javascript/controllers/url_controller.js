4eca4416de52f5547bc940497d5f4cc142b3f147
Quinn Daley <quinn@fishpercolator.co.uk>
2020-06-29 16:56:34 +0100

    New URL box with copy button

---
+++ app/javascript/controllers/url_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ['url', 'copyButton']
+  
+  connect () {
+    if (this.urlTarget) {
+      this.url = this.urlTarget.innerHTML
+    }
+  }
+  
+  copy () {
+    navigator.permissions.query({name: 'clipboard-write'}).then(result => {
+      if (result.state == "granted" || result.status == "prompt") {
+        navigator.clipboard.writeText(this.url).then(() => {
+          this.copyButtonTarget.classList.add('copy-button--copied')
+        })
+      }
+    })
+  }
+  
+}
