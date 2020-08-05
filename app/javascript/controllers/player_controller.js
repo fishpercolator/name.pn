2080b55ab339c053723b5ca174118ddbe8b75204
Quinn Daley <quinn@fishpercolator.co.uk>
2020-08-05 10:52:18 +0100

    Restore player to native, remove ogv

--- app/javascript/controllers/player_controller.js
+++ app/javascript/controllers/player_controller.js
@@ -1,32 +1,27 @@
 import { Controller } from "stimulus"
 
-let ogv = require('ogv')
-ogv.OGVLoader.base = '/ogv'
-
 export default class extends Controller {
   static targets = ['media', 'play']
   
   connect () {
     this.element.addEventListener('new-url', () => { this.refresh() })
-    this.player = new ogv.OGVPlayer()
-    this.player.addEventListener('ended', () => { this.playTarget.disabled = false })
-    this.mediaTarget.appendChild(this.player)
+    this.mediaTarget.addEventListener('ended', () => { this.playTarget.disabled = false })
     this.refresh()
   }
   
   refresh () {
     let url = this.data.get('url')
     if (url) {
-      this.player.src = url
+      this.mediaTarget.src = url
       this.playTarget.disabled = false
     } else {
-      this.player.removeAttribute('src')
+      this.mediaTarget.removeAttribute('src')
       this.playTarget.disabled = true
     }
   }
   
   play () {
-    this.player.play()
+    this.mediaTarget.play()
     this.playTarget.disabled = true
   }
 }
