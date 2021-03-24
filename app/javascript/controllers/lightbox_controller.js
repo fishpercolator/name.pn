546b485e381c37ca6184ffdeebe4867180599393
Quinn Daley <quinn@fishpercolator.co.uk>
2021-03-24 16:44:01 +0000

    Make Lighthouse score better and fix Luminous

--- app/javascript/controllers/lightbox_controller.js
+++ app/javascript/controllers/lightbox_controller.js
@@ -4,8 +4,6 @@ import { Luminous } from 'luminous-lightbox'
 
 export default class extends Controller {
   connect () {
-    this.lightbox = new Luminous(this.element, {
-      injectBaseStyles: false
-    })
+    this.lightbox = new Luminous(this.element)
   }
 }
