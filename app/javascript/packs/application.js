8099af497646e6a7f40ffba4ef2e8dd052324574
Quinn Daley <quinn@fishpercolator.co.uk>
2021-05-19 14:46:08 +0100

    fix up some webpacker bits

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,6 +18,3 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("controllers", true, /.js$/)
 application.load(definitionsFromContext(context))
-
-// Import SCSS
-import '../scss/application.scss'
