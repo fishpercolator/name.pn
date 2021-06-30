f02342aed93067f366b5eba506db22e5341cd6c1
Quinn Daley <quinn@fishpercolator.co.uk>
2021-06-30 17:41:18 +0100

    Fill out missing policy spec

--- spec/policies/client_policy_spec.rb
+++ spec/policies/client_policy_spec.rb
@@ -1,27 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe ClientPolicy, type: :policy do
-  let(:user) { User.new }
-
-  subject { described_class }
-
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(user, client) }
+  let!(:client) { create :client }
+  
+  let(:resolved_scope) do
+    described_class::Scope.new(user, Client.all).resolve
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  context 'being anonymous' do
+    let(:user) { nil }
+    it { is_expected.to forbid_actions(%i[create show update destroy]) }
+    it { expect(resolved_scope).to be_empty }
+  end
+  
+  context 'being the user associated with this client' do
+    let(:user) { client.user }
+    it { is_expected.to permit_actions(%i[create destroy]) }
+    it { is_expected.to permit_mass_assignment_of(%i[name]) }
+    it { is_expected.not_to permit_mass_assignment_of(%i[user_id jti created_at]) }
+    it { expect(resolved_scope).to include(client) }
+  end
+  
+  context 'being another user' do
+    let(:user) { create :user }
+    it { is_expected.to forbid_actions(%i[create show update destroy]) }
+    it { expect(resolved_scope).to be_empty }
   end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
 end
