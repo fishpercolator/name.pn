7f805cfa3fcee52cacc9335ac7664edeb00317a0
Quinn Daley <quinn@fishpercolator.co.uk>
2021-09-22 17:19:55 +0100

    Show variants on dashboard

--- app/models/alternate_name.rb
+++ app/models/alternate_name.rb
@@ -1,6 +1,6 @@
 class AlternateName < ApplicationRecord
   belongs_to :user
-  enum category: {like: 'like', sometimes: 'sometimes', ok: 'ok', dislike: 'dislike'}, _prefix: true
+  enum category: {like: 'like', ok: 'ok', sometimes: 'sometimes', dislike: 'dislike'}, _prefix: true
   
   validates :name, :category, presence: true
 end
