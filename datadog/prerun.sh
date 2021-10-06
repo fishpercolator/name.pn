f731168496bbe154c29fbdd0e5171621de85426a
Quinn Daley <quinn@fishpercolator.co.uk>
2021-10-06 16:47:22 +0100

    Try moving the postgres config

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Disable the Datadog Agent based on dyno type
-if [ "$DYNOTYPE" == "run" ]; then
+if [ "$DYNOTYPE" == "run" ] || [ "$DYNOTYPE" == "scheduler" ] || [ "$DYNOTYPE" == "release" ]; then
   DISABLE_DATADOG_AGENT="true"
 fi
 
@@ -14,13 +14,10 @@ fi
 if [ -n "$DATABASE_URL" ]; then
   POSTGREGEX='^postgres://([^:]+):([^@]+)@([^:]+):([^/]+)/(.*)$'
   if [[ $DATABASE_URL =~ $POSTGREGEX ]]; then
-    sed -i "s/<YOUR HOSTNAME>/${BASH_REMATCH[3]}/" "$APP_DATADOG_CONF_DIR/postgres.d/conf.yaml"
-    sed -i "s/<YOUR USERNAME>/${BASH_REMATCH[1]}/" "$APP_DATADOG_CONF_DIR/postgres.d/conf.yaml"
-    sed -i "s/<YOUR PASSWORD>/${BASH_REMATCH[2]}/" "$APP_DATADOG_CONF_DIR/postgres.d/conf.yaml"
-    sed -i "s/<YOUR PORT>/${BASH_REMATCH[4]}/" "$APP_DATADOG_CONF_DIR/postgres.d/conf.yaml"
-    sed -i "s/<YOUR DBNAME>/${BASH_REMATCH[5]}/" "$APP_DATADOG_CONF_DIR/postgres.d/conf.yaml"
+    sed -i "s/<YOUR HOSTNAME>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+    sed -i "s/<YOUR USERNAME>/${BASH_REMATCH[1]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+    sed -i "s/<YOUR PASSWORD>/${BASH_REMATCH[2]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+    sed -i "s/<YOUR PORT>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+    sed -i "s/<YOUR DBNAME>/${BASH_REMATCH[5]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
   fi
 fi
-
-# Install the "ping" community integration
-agent-wrapper integration install -t datadog-ping==1.0.0
