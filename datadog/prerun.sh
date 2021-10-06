177042d49671d6c9151f1087321a02284ed9d40e
Quinn Daley <quinn@fishpercolator.co.uk>
2021-10-06 11:27:30 +0100

    Add a datadog prerun and postgres monitoring - hopefully

---
+++ datadog/prerun.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Disable the Datadog Agent based on dyno type
+if [ "$DYNOTYPE" == "run" ]; then
+  DISABLE_DATADOG_AGENT="true"
+fi
+
+# Set app version based on HEROKU_SLUG_COMMIT
+if [ -n "$HEROKU_SLUG_COMMIT" ]; then
+  DD_VERSION=$HEROKU_SLUG_COMMIT
+fi
+
+# Update the Postgres configuration from above using the Heroku application environment variable
+if [ -n "$DATABASE_URL" ]; then
+  POSTGREGEX='^postgres://([^:]+):([^@]+)@([^:]+):([^/]+)/(.*)$'
+  if [[ $DATABASE_URL =~ $POSTGREGEX ]]; then
+    sed -i "s/<YOUR HOSTNAME>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+    sed -i "s/<YOUR USERNAME>/${BASH_REMATCH[1]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+    sed -i "s/<YOUR PASSWORD>/${BASH_REMATCH[2]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+    sed -i "s/<YOUR PORT>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+    sed -i "s/<YOUR DBNAME>/${BASH_REMATCH[5]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+  fi
+fi
+
+# Install the "ping" community integration
+agent-wrapper integration install -t datadog-ping==1.0.0
