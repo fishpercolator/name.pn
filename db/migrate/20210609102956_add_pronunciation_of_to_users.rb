bf19f6fd61598fdd5c412ba14b584dc250e10b20
Quinn Daley <quinn@fishpercolator.co.uk>
2022-03-23 18:07:39 +0000

    Retrospectively fix migrations for enums

--- db/migrate/20210609102956_add_pronunciation_of_to_users.rb
+++ db/migrate/20210609102956_add_pronunciation_of_to_users.rb
@@ -1,7 +1,7 @@
 class AddPronunciationOfToUsers < ActiveRecord::Migration[6.1]
   def up
     create_enum :user_name_variant, %i[full_name personal_name formal_name envelope_name]
-    add_column :users, :pronunciation_of, :user_name_variant, null: false, default: :full_name
+    add_column :users, :pronunciation_of, :enum, enum_type: :user_name_variant, null: false, default: :full_name
   end
   def down
     remove_column :users, :pronunciation_of
