cd49aaeed18e37c64f61cfbfb6710122752728cd
Quinn Daley <quinn@fishpercolator.co.uk>
2022-03-23 17:47:17 +0000

    First attempt at upgrading to Rails 7. Needs some deprecation warnings addressing and then the big task of removing Webpack in favour of hotwire etc

--- vendor/prepare-env.sh
+++ vendor/prepare-env.sh
@@ -8,7 +8,7 @@ echo "deb http://ftp.debian.org/debian buster main" > /etc/apt/sources.list.d/bu
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -\
     && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt-get update -y && apt-get install -y chromium nodejs postgresql-client-11 vim yarn && apt-get clean
+apt-get update -y && apt-get install -y chromium iproute2 nodejs postgresql-client-11 vim yarn && apt-get clean
 
 # Create a user
 adduser rails --uid $uid --disabled-password --gecos ""
