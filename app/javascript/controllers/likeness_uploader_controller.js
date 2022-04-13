e297da102313927c098f60661e0446c0d419a4b0
Quinn Daley <quinn@fishpercolator.co.uk>
2022-04-13 17:58:03 +0100

    The first part of switching from unmaintained uppload to uppy, which provides many of the same features

--- app/javascript/controllers/likeness_uploader_controller.js
+++ app/javascript/controllers/likeness_uploader_controller.js
@@ -1,48 +1,66 @@
 import { Controller } from "stimulus"
 
-import { Uppload, Local, Camera, Crop, Rotate, Brightness, Contrast, xhrUploader, en } from "uppload";
+import Uppy from '@uppy/core'
+import XHRUpload from '@uppy/xhr-upload'
+import Dashboard from '@uppy/dashboard'
+import ImageEditor from '@uppy/image-editor'
 import Rails from 'rails-ujs'
 
 export default class extends Controller {
   static targets = ['preview', 'delete']
   
   connect() {
-    this.uploader = new Uppload({
-      lang: en,
-      defaultService: 'local',
-      compression: 0.8,
-      maxSize: [1024, 1024],
-      uploader: xhrUploader({
-        endpoint: '/profile/likeness',
-        fileKeyName: 'user[likeness]',
-        method: 'PATCH',
-        settingsFunction: xmlHttp => {
-          xmlHttp.setRequestHeader('Accept', 'application/json')
-          xmlHttp.setRequestHeader('X-CSRF-Token', Rails.csrfToken())
-        }
-      })
+    this.uploader = new Uppy({
+      restrictions: {
+        maxNumberOfFiles: 1
+      }
     })
-    this.uploader.use([
-      new Local(),
-      new Camera(),
-      new Crop({
+    .use(Dashboard, {
+      autoOpenFileEditor: true,
+      showSelectedFiles: false
+    })
+    .use(ImageEditor, {
+      target: Dashboard,
+      quality: 0.8,
+      cropperOptions: {
         aspectRatio: 1
-      }),
-      new Rotate(),
-      new Brightness(),
-      new Contrast()
-    ])
-    this.uploader.on('upload', url => {
-      let img = document.createElement('img')
-      img.setAttribute('src', url)
-      this.previewTarget.innerHTML = ''
-      this.previewTarget.appendChild(img)
-      this.deleteTarget.disabled = false
+      },
+      actions: {
+        revert: false,
+        granularRotate: false,
+        flip: false,
+        cropSquare: false,
+        cropWidescreen: false,
+        cropWidescreenVertical: false
+      }
+    })
+    .use(XHRUpload, {
+      endpoint: '/profile/likeness',
+      method: 'patch',
+      fieldName: 'user[likeness]',
+      headers: {
+        accept: 'application/json',
+        'x-csrf-token': Rails.csrfToken()
+      }
+    })
+    this.uploader.on('file-editor:complete', () => {
+      this.uploader.upload()
+    })
+    this.uploader.on('complete', result => {
+      if (result.successful.length > 0) {
+        let img = document.createElement('img')
+        img.setAttribute('src', result.successful[0].uploadURL)
+        this.previewTarget.innerHTML = ''
+        this.previewTarget.appendChild(img)
+        this.deleteTarget.disabled = false
+        this.uploader.getPlugin('Dashboard').closeModal()
+      }
     })
   }
   
   start() {
-    this.uploader.open()
+    this.uploader.reset()
+    this.uploader.getPlugin('Dashboard').openModal()
   }
   
 }
