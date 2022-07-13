3a656f562294bfad6769bff8efb8a42419afb707
Quinn Daley <quinn@fishpercolator.co.uk>
2022-07-13 17:24:40 +0100

    If we've hit the ErrorsController we know we need an HTML page so override format

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,7 +1,15 @@
 class ErrorsController < ApplicationController
   include Gaffe::Errors
+  
+  before_action :format_html
 
   skip_after_action :verify_authorized
 
   layout 'application'
+  
+  private
+  
+  def format_html
+    request.format = :html
+  end
 end
