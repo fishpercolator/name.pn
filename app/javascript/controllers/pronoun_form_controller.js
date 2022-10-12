2287e56181a78f2f4c700f7880e022d7bd5d52ff
Quinn Daley <quinn@fishpercolator.co.uk>
2022-10-12 14:15:32 +0100

    Make reordering work in JS

--- app/javascript/controllers/pronoun_form_controller.js
+++ app/javascript/controllers/pronoun_form_controller.js
@@ -1,16 +1,25 @@
 import { Controller } from "stimulus"
-// import Sortable from 'sortablejs'
+import Sortable from 'sortablejs'
 
 export default class extends Controller {
-  static targets = ['pronounless', 'list', 'option']
+  static targets = ['pronounless', 'list', 'option', 'position']
 
   connect () {
-    // this.sortable = Sortable.create(this.listTarget)
     this.changePronounless()
+    this.fillPositions()
+    this.sortable = Sortable.create(this.listTarget, {onUpdate: () => this.fillPositions()})
+    this.positionTargets.forEach(t => t.style.display = 'none')
   }
 
   changePronounless() {
     let disabled = (this.pronounlessTarget.value !== '')
     this.optionTargets.forEach(t => t.disabled = disabled)
   }
+
+  fillPositions() {
+    let pos = 1
+    this.positionTargets.forEach(t => t.value = pos++)
+  }
+
+  
 }
