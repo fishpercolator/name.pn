d28a7cf896463ec029050cc774099fb0223fc1d7
Quinn Daley <quinn@fishpercolator.co.uk>
2022-10-19 17:23:27 +0100

    First scratching around at a total revamp of the audio recorder

--- app/javascript/controllers/recorder_controller.js
+++ app/javascript/controllers/recorder_controller.js
@@ -1,95 +1,113 @@
 import { Controller } from "@hotwired/stimulus"
 
-import Recorder from 'opus-recorder'
+// import Recorder from 'opus-recorder'
 
 export default class extends Controller {
-  static targets = [
-    'field', 'button', 'player', 
-    'delete', 'deleteFlag'
-  ]
+  static targets = ['field']
+
+  // static targets = [
+  //   'field', 'button', 'player', 
+  //   'delete', 'deleteFlag'
+  // ]
   static classes = [
-    'enabled'
+    'granted', 'prompt', 'denied'
   ]
+
+  connect () {
+    this.testMicState()
+  }
+
+  // Test the current state of the mic and set classes on the element accordingly
+  async testMicState() {
+    this.micState = await navigator.permissions.query({ name: 'microphone' })
+    // Remove any classes that were added by earlier calls to this method
+    const classes = [this.grantedClass, this.promptClass, this.deniedClass]
+    classes.forEach(x => this.element.classList.remove(x))
+    // Set the appropriate class for the current micState
+    if (this.micState.state) {
+      this.element.classList.add(this[this.micState.state + 'Class'])
+    }
+  }
   
-  connect () {    
-    // Recorder is hidden in CSS - show it if we detect the browser supports it
-    if (Recorder.isRecordingSupported()) {
-      this.element.classList.add(this.enabledClass)
+  // connect () {    
+  //   // Recorder is hidden in CSS - show it if we detect the browser supports it
+  //   if (Recorder.isRecordingSupported()) {
+  //     this.element.classList.add(this.enabledClass)
       
-      this.initializeRecorder()
-      this.initializeFileReader()
+  //     this.initializeRecorder()
+  //     this.initializeFileReader()
       
-      if (this.playerTarget.dataset['playerUrlValue']) {
-        this.deleteTarget.disabled = false
-      }
-    }
-  }
+  //     if (this.playerTarget.dataset['playerUrlValue']) {
+  //       this.deleteTarget.disabled = false
+  //     }
+  //   }
+  // }
   
-  initializeRecorder () {
-    this.recorder = new Recorder({
-      encoderPath: '/workers/waveWorker.min.js',
-      streamPages: false,
-      numberOfChannels: 1,
-      wavBitDepth: 16
-    })
-    this.recorder.onstart = () => { this.buttonTarget.classList.add('is-active') }
-    this.recorder.onstop = () => { this.buttonTarget.classList.remove('is-active') }
-    this.recorder.ondataavailable = (arrayBuffer) => { this.processNewAudioData(arrayBuffer) }
-  }
+  // initializeRecorder () {
+  //   this.recorder = new Recorder({
+  //     encoderPath: '/workers/waveWorker.min.js',
+  //     streamPages: false,
+  //     numberOfChannels: 1,
+  //     wavBitDepth: 16
+  //   })
+  //   this.recorder.onstart = () => { this.buttonTarget.classList.add('is-active') }
+  //   this.recorder.onstop = () => { this.buttonTarget.classList.remove('is-active') }
+  //   this.recorder.ondataavailable = (arrayBuffer) => { this.processNewAudioData(arrayBuffer) }
+  // }
   
-  initializeFileReader () {
-    this.reader = new FileReader ()
-    this.reader.onload = (e) => {
-      this.fieldTarget.value = this.reader.result
-    }
-  }
+  // initializeFileReader () {
+  //   this.reader = new FileReader ()
+  //   this.reader.onload = (e) => {
+  //     this.fieldTarget.value = this.reader.result
+  //   }
+  // }
   
-  // FIXME: Catch user holding record button for too long etc
-  start (e) {
-    e.preventDefault()
-    try {
-      this.recorder.start()
-    } catch(e) {
-      console.log('Error: ' + e.message)
-    }
-  }
+  // // FIXME: Catch user holding record button for too long etc
+  // start (e) {
+  //   e.preventDefault()
+  //   try {
+  //     this.recorder.start()
+  //   } catch(e) {
+  //     console.log('Error: ' + e.message)
+  //   }
+  // }
   
-  stop (e) {
-    e.preventDefault()
-    this.recorder.stop()
-  }
+  // stop (e) {
+  //   e.preventDefault()
+  //   this.recorder.stop()
+  // }
   
-  processNewAudioData (recording) {
-    const rec_blob = new Blob([recording])
+  // processNewAudioData (recording) {
+  //   const rec_blob = new Blob([recording])
     
-    // This sets the file for upload
-    this.reader.readAsDataURL(rec_blob)
+  //   // This sets the file for upload
+  //   this.reader.readAsDataURL(rec_blob)
     
-    // This uses a blob locally for the preview player
-    const rec_url = URL.createObjectURL(rec_blob)
-    this.setPlayerUrl(rec_url)
+  //   // This uses a blob locally for the preview player
+  //   const rec_url = URL.createObjectURL(rec_blob)
+  //   this.setPlayerUrl(rec_url)
     
-    // And finally, make sure the delete button is active
-    this.deleteFlagTarget.value = ''
-    this.deleteTarget.disabled = false
-  }
+  //   // And finally, make sure the delete button is active
+  //   this.deleteFlagTarget.value = ''
+  //   this.deleteTarget.disabled = false
+  // }
   
-  delete () {
-    this.deleteFlagTarget.value = '1'
-    this.deleteTarget.disabled = true
-    this.setPlayerUrl(null)
-  }
+  // delete () {
+  //   this.deleteFlagTarget.value = '1'
+  //   this.deleteTarget.disabled = true
+  //   this.setPlayerUrl(null)
+  // }
   
-  setPlayerUrl (url) {
-    if (url === null) {
-      delete this.playerTarget.dataset.playerUrlValue
-    } else {
-      this.playerTarget.dataset.playerUrlValue = url
-    }
-  }
+  // setPlayerUrl (url) {
+  //   if (url === null) {
+  //     delete this.playerTarget.dataset.playerUrlValue
+  //   } else {
+  //     this.playerTarget.dataset.playerUrlValue = url
+  //   }
+  // }
   
-  // Get a dataURL for the given arrayBuffer
-  dataUrl(buf) {
-    return 'data:audio/ogg;codecs=opus;base64,' + btoa(String.fromCharCode.apply(null, new Uint8Array(buf)))
-  }
+  // // Get a dataURL for the given arrayBuffer
+  // dataUrl(buf) {
+  //   return 'data:audio/ogg;codecs=opus;base64,' + btoa(String.fromCharCode.apply(null, new Uint8Array(buf)))
+  // }
 }
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -12,7 +12,7 @@ class UserPolicy < ApplicationPolicy
     [
       :full_name, :personal_name, :formal_name, :envelope_name,
       :phonetic, :ipa, :slug, :likeness, :pronoun_example, :pronoun_style,
-      :pronounless_style, :pronunciation_of, :noindex,
+      :pronounless_style, :pronunciation, :pronunciation_of, :noindex,
       :user_pronoun_sets_attributes => [:id, :pronoun_set_id, :position, :_destroy],
       :links_attributes => [:id, :title, :url, :_destroy], 
       :alternate_names_attributes => [:id, :name, :category, :_destroy]
