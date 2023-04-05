54bc198ad09d0ee26ea7b81d808dbf5525f9225f
Quinn <quinn@fishpercolator.co.uk>
2023-04-05 10:33:54 +0100

    Switch to jsbundling/cssbundling (#99)
    * First attempt at moving to esbuild - some things are working and some are not
    
    * OK, it was a silly typo of course
    
    * Remove some other unneeded things
    
    * Fix serviceworker
    
    * Disable debug assets
    
    * Cache the logo in the serviceworker
    
    * Trigger actions on pull_request
    
    * Ensure we actually import the uppy styles rather than linking to them
    
    * Add asset build to test pipeline
    
    * Change to a version of bulma-o-steps without errors
    
    * Remove running the tests on PR, which was added during an outage last week and is causing duplicate runs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -16,8 +16,4 @@ if (navigator.serviceWorker) {
 }
 
 // Start Stimulus
-import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
-window.Stimulus = Application.start()
-const context = require.context("controllers", true, /.js$/)
-Stimulus.load(definitionsFromContext(context))
+import "./controllers";
