f2b83e66b3766df20b57f11af3ecfedf40773116
Quinn Daley <quinn@fishpercolator.co.uk>
2023-07-19 17:23:37 +0100

    Implement the full functionality for linkable sections - but needs tests

--- app/javascript/controllers/public_profile_controller.js
+++ app/javascript/controllers/public_profile_controller.js
@@ -7,18 +7,33 @@ export default class extends Controller {
   }
 
   connect () {
-    this.hashValue = window.location.hash
+    this.hashChange()
   }
   hashChange () {
     this.hashValue = window.location.hash
   }
   hashValueChanged(value) {
-    this.cardTargets.forEach(c => {
-      if (this.hashValue === `#${c.id}` || this.hashValue === '') {
-        c.style.display = 'block'
+    let cardToShow
+    if (cardToShow = this.cardTargets.find(x => value === `#${x.id}`)) {
+      this.showCard(cardToShow)
+    } else {
+      this.showAllCards()
+    }
+  }
+  // Given a card, show only that card
+  showCard(card) {
+    this.cardTargets.forEach(x => {
+      if (x === card) {
+        x.classList.remove('is-hidden')
+        x.classList.add('is-shown-alone')
       } else {
-        c.style.display = 'none'
+        x.classList.add('is-hidden')
+        x.classList.remove('is-shown-alone')
       }
     })
+  }
+  // Show all cards
+  showAllCards() {
+    this.cardTargets.forEach(x => x.classList.remove('is-hidden', 'is-shown-alone'))
   }
 }
