5bba8643346768ebe88575730157fa36ba172b7a
Quinn Daley <quinn@fishpercolator.co.uk>
2025-05-06 15:14:03 +0100

    First attempt at ditching turbolinks and render_async for turbo

--- app/controllers/mailing_list_controller.rb
+++ app/controllers/mailing_list_controller.rb
@@ -3,21 +3,16 @@ class MailingListController < ApplicationController
   skip_after_action :verify_authorized
   
   def show
-    render partial: 'mailing_list_form'
   end
   
   def unsubscribe
     current_user.unsubscribe_from_mailing_list!
-    respond_to do |format|
-      format.js { render 'rerender' }
-    end
+    render :show
   end
   
   def subscribe
     current_user.subscribe_to_mailing_list!
-    respond_to do |format|
-      format.js { render 'rerender' }
-    end
+    render :show
   end
   
 end
