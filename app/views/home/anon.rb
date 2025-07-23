926b879438acc34a9b08ed7133bed1c84d4bc2ef
Quinn Daley <quinn@fishpercolator.co.uk>
2025-07-23 10:11:23 +0100

    At least compile

--- app/views/home/anon.rb
+++ app/views/home/anon.rb
@@ -2,7 +2,7 @@
 
 class Views::Home::Anon < Views::Base
   def view_template
-    ShellLayout(title: "Home") do
+    ShellLayout do
       h1 { "This is the homepage" }
       Button { "Hello" }
     end
