01cc4211a9dca42e7857d6079477c6df44edfa62
Quinn Daley <quinn@fishpercolator.co.uk>
2025-07-24 15:11:28 +0100

    Set the font to Raleway

--- app/components/layout.rb
+++ app/components/layout.rb
@@ -30,9 +30,16 @@ class Components::Layout < Components::Base
       favicon_link_tag 'apple-touch-icon.png', rel: 'apple-touch-icon', type: 'image/png'
       link(rel: 'manifest', href: manifest_path(format: :json))
       javascript_include_tag 'application', data_turbo_track: 'reload', type: 'module'
+      google_fonts
       stylesheet_link_tag 'tailwind', media: 'all', data_turbo_track: 'reload'
       render partial('layouts/ga')
     end
+  end
+
+  def google_fonts
+    link(rel: 'preconnect', href: 'https://fonts.googleapis.com')
+    link(rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true)
+    link(href: 'https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap', rel: 'stylesheet')
   end
 
   def default_meta_tags
