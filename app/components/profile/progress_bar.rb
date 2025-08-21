299f940725d2960e69671041c96f1de49364df1b
Quinn Daley <quinn@fishpercolator.co.uk>
2025-08-21 16:37:50 +0100

    Add the link (still needs to be disabled for incomplete profiles)

--- app/components/profile/progress_bar.rb
+++ app/components/profile/progress_bar.rb
@@ -17,19 +17,24 @@ class Components::Profile::ProgressBar < Components::Base
 
   # Thanks to https://ishadeed.com/article/stepper-component-html-css/ for the inspiration
   def step(name)
-    li(**mix({class: 'flex-1 flex sm:flex-col items-center before:h-6 before:w-6 before:relative before:z-1 before:rounded-full before:m-2 after:w-1 after:h-8 sm:after:w-full sm:after:h-1 after:-order-1 after:relative after:top-4 sm:after:top-[22px] after:left-[22px] sm:after:left-[50%] last:after:invisible'}, {class: step_class(name)})) do
-      Link(href: helpers.wizard_path(name)) { t("profile.#{name}.title") }
+    li(**mix({class: 'flex-1 flex sm:flex-col items-center after:w-1 after:h-8 sm:after:w-full sm:after:h-1 after:-order-1 after:relative after:top-4 sm:after:top-[22px] after:left-[22px] sm:after:left-[50%] last:after:invisible'}, {class: connector_class(name)})) do
+      Link(**mix({class: 'flex sm:flex-col items-center flex-1 p-0 before:h-6 before:w-6 before:relative before:z-1 before:rounded-full before:m-2'}, {class: pip_class(name)}), href: helpers.wizard_path(name)) { t("profile.#{name}.title") }
     end
   end
 
   # If the step is in the future, return a different colour
-  def step_class(name)
-    if current_step == name
-      'before:bg-primary after:bg-slate-300 after:dark:bg-slate-700'
-    elsif future_steps.include? name
-      'before:bg-slate-300 before:dark:bg-slate-700 after:bg-slate-300 after:dark:bg-slate-700'
+  def connector_class(name)
+    if current_step == name || future_steps.include?(name)
+      'after:bg-slate-300 after:dark:bg-slate-700'
     else
-      'before:bg-primary after:bg-primary'
+      'after:bg-primary'
+    end
+  end
+  def pip_class(name)
+    if future_steps.include?(name)
+      'before:bg-slate-300 before:dark:bg-slate-700'
+    else
+      'before:bg-primary'
     end
   end
 
